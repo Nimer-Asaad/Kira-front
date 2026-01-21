315d8bf367991fa4dcfd25490aa490ae5ac5d8e4
Nimer Asaad <nimerziad46@gmail.com>
2026-01-21 22:16:27 +0200

    fix some bug

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
+    host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,
   },
 })
